a46ba11c24397732508c4f55f93e5d42b29f0d5e
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-08 22:59:00 +0000

    some basic CSS added, last FM Class complete

--- public/index.js
+++ public/index.js
@@ -2,31 +2,69 @@ var app = function(){
 
   var lastFM = new LastFm();
   var topArtistsUrl = lastFM.setCategoryType('gettopartists');
+  var topTracksUrl = lastFM.setCategoryType('gettoptracks');
+  var weeklyTracksUrl = lastFM.setCategoryType('getweeklytrackchart');
 
-  makeTopArtistRequest(topArtistsUrl, requestComplete);
+  var userInfo = lastFM.setCategoryType('getinfo');
+
+
+  // makeRequest(topArtistsUrl, artistRequestComplete);
+  // makeRequest(topTracksUrl, trackRequestComplete);
+  // makeRequest(weeklyTracksUrl, weeklyTracksComplete);
+  makeRequest(userInfo, userInformationComplete)
+
 }
 
-var makeTopArtistRequest = function(url, callback){
-  var request = new XMLHttpRequest();
-  request.open("GET", url);
-  request.addEventListener("load", callback);
-  request.send();
-}
+// var makeRequest = function(url, callback){
+//   var request = new XMLHttpRequest();
+//   request.open("GET", url);
+//   request.addEventListener("load", callback);
+//   request.send();
+// }
 
-var requestComplete = function(){
+var trackRequestComplete = function(){
   if (this.status !== 200) return;
   var jsonString = this.responseText;
-  var myTopArtists = JSON.parse(jsonString);
-  populateArtistList(myTopArtists.topartists.artist);
+  var myData = JSON.parse(jsonString);
+  populateTrackList(myData.toptracks.track);
 }
 
-var populateArtistList = function(myTopArtists){
+var artistRequestComplete = function(){
+  if (this.status !== 200) return;
+  var jsonString = this.responseText;
+  var myData = JSON.parse(jsonString);
+  populateArtistList(myData.topartists.artist);
+}
+
+var populateArtistList = function(myLastFmData){
   var main = document.getElementById('main-content');
   var ul = document.getElementById('artist-list');
 
-  myTopArtists.forEach(function(artist){
+  myLastFmData.forEach(function(artist){
     var li = document.createElement('li');
-    li.innerText = artist.name
+    li.innerText = artist.name + " (" + artist.playcount + ")";
+    ul.appendChild(li);
+  })
+}
+
+var populateTrackList = function(myLastFmData){
+  var main = document.getElementById('track-content');
+  var ul = document.getElementById('track-list');
+
+  myLastFmData.forEach(function(track){
+    var li = document.createElement('li');
+    li.innerText = track.name + " (" + track.playcount + ")";
+    ul.appendChild(li);
+  })
+}
+
+var userInformationComplete = function(myLastFmData){
+  var tracks = document.getElementById('total-tracks-played');
+  var ul = document.getElementById('track-list');
+
+  myLastFmData.forEach(function(track){
+    var li = document.createElement('li');
+    li.innerText = track.name + " (" + track.playcount + ")";
     ul.appendChild(li);
   })
 }
