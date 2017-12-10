42746aef75aab6ec5cea733092812cab088bd71a
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-10 21:22:46 +0000

    top albums html written

--- public/index.js
+++ public/index.js
@@ -3,13 +3,15 @@ var app = function(){
   var lastFM = new LastFm();
   var topArtistsUrl = lastFM.setCategoryType('gettopartists');
   var topTracksUrl = lastFM.setCategoryType('gettoptracks&limit=25');
-  var recentlyPlayedTracks = lastFM.setCategoryType('getRecentTracks&limit=10')
+  var recentlyPlayedTracks = lastFM.setCategoryType('getRecentTracks&limit=10');
+  var topAlbumsUrl = lastFM.setCategoryType('user.gettopalbums');
   var userInfo = lastFM.setCategoryType('getinfo');
 
   makeRequest(topArtistsUrl, artistRequestComplete);
   makeRequest(topTracksUrl, trackRequestComplete);
-  makeRequest(userInfo, userRequestComplete)
-  makeRequest(recentlyPlayedTracks, recentTrackRequestComplete)
+  makeRequest(userInfo, userRequestComplete);
+  makeRequest(recentlyPlayedTracks, recentTrackRequestComplete);
+  makeRequest(topAlbumsUrl, albumsRequestComplete);
 
 }
 
@@ -18,6 +20,15 @@ var makeRequest = function(url, callback){
   request.open("GET", url);
   request.addEventListener("load", callback);
   request.send();
+}
+
+
+var albumRequestComplete = function(){
+  if (this.status !== 200) return;
+  var jsonString = this.responseText;
+  var myData = JSON.parse(jsonString);
+  // changeTrackByDate(myData.toptracks.track);
+  populateAlbumList(myData.toptracks.track);
 }
 
 var trackRequestComplete = function(){
@@ -147,6 +158,52 @@ var populateArtistList = function(artistList){
   var artistFourImage = document.getElementById('artist-four-image').src = topFour[2].image[2]['#text'];
   var artistFiveImage = document.getElementById('artist-five-image').src = topFour[3].image[2]['#text'];
 }
+
+
+var populateAlbumList = function(artistList){
+
+  var artistBlock = document.getElementById('artist-block');
+  var headerDetails = document.getElementById('header-details');
+
+  var topFive = artistList.splice(0, 5);
+  var topFour = topFive.splice(1, 5);
+
+  var artist1 = document.getElementById('artist1');
+  var artist1name = document.getElementById('top-artist-square-name');
+  var artist1playcount = document.getElementById('top-artist-square-playcount');
+
+  var artist2 = document.getElementById('artist2');
+  var artist3 = document.getElementById('artist3');
+  var artist4 = document.getElementById('artist4');
+  var artist5 = document.getElementById('artist5');
+
+  var smallArtistsBoxName = document.getElementsByClassName('small-artist-square-name');
+  var smallArtistsBoxPlaycount = document.getElementsByClassName('small-artist-square-playcount');
+
+  artist1name.innerText = topFive[0].name;
+  artist1playcount.innerText = topFive[0].playcount + " plays";
+
+  artist2name.innerText = topFour[0].name;
+  artist2playcount.innerText = topFour[0].playcount + " plays";
+
+  artist3name.innerText = topFour[1].name;
+  artist3playcount.innerText = topFour[1].playcount + " plays";
+
+  artist4name.innerText = topFour[2].name;
+  artist4playcount.innerText = topFour[2].playcount + " plays";
+
+  artist5name.innerText = topFour[3].name;
+  artist5playcount.innerText = topFour[3].playcount + " plays";
+
+  var artistOneImage = document.getElementById('top-artist-image').src = topFive[0].image[3]['#text'];
+  var artistTwoImage = document.getElementById('artist-two-image').src = topFour[0].image[2]['#text'];
+  var artistThreeImage = document.getElementById('artist-three-image').src = topFour[1].image[2]['#text'];
+  var artistFourImage = document.getElementById('artist-four-image').src = topFour[2].image[2]['#text'];
+  var artistFiveImage = document.getElementById('artist-five-image').src = topFour[3].image[2]['#text'];
+}
+
+
+
 
 var populateRecentTrackList = function(recentTracks){
   var ul = document.getElementById('recent-track-list');
